5e50a500f443defe41048d8533a3f8cde741321e
podpah <pedrinhohrodrigues@outlook.com>
2022-09-20 17:58:55 +0100

    Initial commit

---
+++ src/main/java/com/example/hol/CreateTable.java
@@ -0,0 +1,37 @@
+package com.example.hol;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTable {
+
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:D:\\Programming\\Saves\\Java\\HoL\\src\\main\\java\\com\\example\\hol\\sqlite\\scoreb.db";
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS scoreboard (\n"
+                + " id integer PRIMARY KEY,\n"
+                + " name text NOT NULL,\n"
+                + " score integer\n"
+                + ");";
+
+        try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        createNewTable();
+    }
+
+}
